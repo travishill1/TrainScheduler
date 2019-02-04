56bfc7d54ed6d2b1606910f61e0cdf94d6b992ef
Travis <travishill1@gmail.com>
2019-02-03 20:37:49 -0600

    base functioning finished, needs design work and bug testing

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,3 +1,14 @@
+// get difference in time between the start time (input) and current time. 
+// convert that difference into minutes = duration
+// duration mod frequency to get TimeRemainder
+
+// 
+// duration = difference in CurrentTime and Start Time(input)
+// TimeRemainder = duration mod frequency.
+// Minutes Away = (frequency - remainder)
+// Next Arrival = Current Time + (Minutes Away)
+
+
 var config = {
     apiKey: "AIzaSyCV9lE2mZCUls3-92WCQpqSXS0YESTxw0Y",
     authDomain: "trainscheduler-48e20.firebaseapp.com",
@@ -18,30 +29,51 @@ var config = {
 
   database.ref().on("child_added",function(childSnap){
       let key = childSnap.key;
-      console.log(key)
+      
       name = childSnap.val().name
-      role = childSnap.val().role
-      date = childSnap.val().date
-      let monthsWorked = moment().diff(date,"Months");
-      rate = childSnap.val().rate
-      let totalBilled = monthsWorked * rate;
-      $("#table-body").append("<tr id='"+ key + "'><td><button key='" + key + "'class='delete'>X</button></td><td>"+ name +"</td><td>"+ role +"</td><td>"+ date +"</td><td>"+ monthsWorked +"</td><td>"+ rate +"</td><td>"+ totalBilled +"</td>")
+      destination = childSnap.val().destination
+      time = childSnap.val().time
+      frequency = childSnap.val().frequency
+
+    let first = moment(time, "HH:mm");
+
+    let currentTime = moment();
+    console.log("current time: " + currentTime);
+
+    let duration = moment.duration(currentTime.diff(first));
+    // let duration = currentTime.diff(moment(timeMinutes), "minutes");
+    console.log("duration: " + duration);
+
+    let timeMinutes = duration.asMinutes();
+    // let timeMinutes = moment(time, "HH:mm").subtract(1, "years");
+    console.log("Time in Minutes: " + timeMinutes);
+
+    let timeRemainder = timeMinutes % frequency;
+    console.log(timeRemainder);
+    
+
+    let minutesAway = Math.floor(frequency - timeRemainder);
+    console.log(minutesAway + " Minutes Away");
+
+    let nextArrival = currentTime.add(minutesAway, "minutes").format("hh:mm a");
+    console.log("Next Arrival Time: " + nextArrival);
+
+
+      $("#table-body").append("<tr id='"+ key + "'><td><button key='" + key + "'class='delete'>X</button></td><td>"+ name +"</td><td>"+ destination +"</td><td>"+ frequency +"</td><td>"+ nextArrival +"</td><td>"+ minutesAway +"</td>")
     })
 
 function addEmployee(event){
     event.preventDefault()
-    let name = $('#name-input').val()
-    let role = $('#role-input').val()
-    let date = moment($('#date-input').val(),"YYYY-MM-DD").format("MM/DD/YYYY")
-    let rate = $('#rate-input').val()    
-
-    console.log(date)
+    let name = $('#name-input').val();
+    let destination = $('#destination-input').val();
+    let time = $('#time-input').val();
+    let frequency = $('#frequency-input').val();  
 
     database.ref().push({
         name,
-        role,
-        date,
-        rate
+        destination,
+        time,
+        frequency
     })
 
 }
